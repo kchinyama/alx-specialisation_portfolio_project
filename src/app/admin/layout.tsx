60bb4c5fa033e56c6aea5dfffdfe44187f40f4cb
Kaumba Chinyama <Kalichi@Kalichis-MacBook-Pro.local>
2024-12-05 14:53:25 +0200

    nav, page and layout pages up and running - might want to rename home page path to /home instaed of /admin

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,28 @@
+/* file that lays out the structure of the 
+home page
+*/
+
+
+import { Nav, NavLink } from "./components/Nav";
+
+
+/*
+ use children attribute to create navigation bar
+for the home page
+*/
+
+export default function AdminLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return <>
+    <Nav>
+        <NavLink href="/admin">Home</NavLink>
+        <NavLink href="admin/products">Products</NavLink>
+        <NavLink href="admin/customers">Customers</NavLink>
+        <NavLink href="admin/orders">Orders</NavLink>
+    </Nav>
+    <div className="container my-6">{children}</div>
+    </>
+}
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,7 @@
+/* 
+boiler plate for all the pages associated with my website
+*/
+
+export default function AdminHomePage() {
+    return <h1>Home Page</h1>
+}
