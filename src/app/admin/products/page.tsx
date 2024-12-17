709e13cb4eba7d41a790200659b1ab719403d5a6
Kaumba Chinyama <Kalichi@Kalichis-MacBook-Pro.local>
2024-12-17 16:07:15 +0200

    new Add Product bug fix

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -8,7 +8,7 @@ export default function HomeProductsPage() {
     <div className="flex justify-between items-center gap-4">
     <PageHeader>Products</PageHeader>
     <Button asChild>
-        <Link href="admin/products/new">Add Product</Link>
+        <Link href="/admin/products/new">Add Product</Link>
     </Button>
     </div>
     <ProductsTable />
