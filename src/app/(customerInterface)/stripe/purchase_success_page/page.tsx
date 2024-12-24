f1a31c4b01810d3ecd260585d7b9c088aaf9d592
Kaumba Chinyama <Kalichi@Kalichis-MacBook-Pro.local>
2024-12-24 23:50:39 +0200

    creating a download link for succesfful purchase

--- src/app/(customerInterface)/stripe/purchase_success_page/page.tsx
+++ src/app/(customerInterface)/stripe/purchase_success_page/page.tsx
@@ -3,6 +3,56 @@ page that displays the successful purchase of an <item />
 like a link to the next page after successful purchase
 */
 
-export default function PurchaseSuccessPage() {
-    return <h1>Hey there successful person</h1>
+import { Button } from "@/components/ui/button";
+import db from "@/db/db";
+import { formatCurrency } from "@/lib/formaters";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+
+export default async function PurchaseSuccessPage({
+    searchParams,
+}: { searchParams: { payment_intent: string}
+
+}) {
+    const paymentIntent = await stripe.paymentIntents.retrieve(searchParams.payment_intent)
+
+    if (paymentIntent.metadata.productID == null) return notFound()
+
+    const product = await db.product.findUnique({ where: {
+        id: paymentIntent.metadata.productID
+    }})
+
+    if (product == null) return notFound()
+
+    const isSuccess = paymentIntent.status === "succeeded"
+
+    return <div className="max-w-5xl w-full mx-auto space-y-8">
+        <h1 className="text-2xl font-bold">
+            {isSuccess ? "Congrats! Payment Successful!": "Sorry, Error Occured!"}
+            </h1>
+    <div className="flex gap-4 items-center">
+        <div className="aspect-video flex-shrink-0 w-1/3 relative">
+            <Image className="object-cover" src={product.imagePath} fill alt={product.name} />
+        </div>
+        <div>
+            <div className="text-lg">
+                {formatCurrency(Number(product.price) / 100)}
+            </div>
+            <h1 className="text-2xl font-bold">{product.name}</h1>
+            <div className="line-clamp-3 text-muted-foreground">{product.description}</div>
+            <Button className="mt-4" size="lg" asChild>
+                {isSuccess ? (
+                    <a></a> ) : (
+                    <Link href={`/products/${product.id}/purchase`}>Please Try Again</Link>
+                    )}
+            </Button>
+        </div>
+    </div>
+
+</div>
 }
